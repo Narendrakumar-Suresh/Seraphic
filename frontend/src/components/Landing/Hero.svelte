fb8920905311995f4c19fa93795bdbcdd0743040
Narendrakumar-Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2025-02-18 06:10:00 +0530

    Completion of Initial landing page

--- frontend/src/components/Landing/Hero.svelte
+++ frontend/src/components/Landing/Hero.svelte
@@ -1,13 +1,17 @@
-<div class="flex items-center justify-center gap-5 pt-24 text-white">
-	<div>
+<div
+	class=" 8 flex items-center justify-center
+gap-16 pt-24 text-white"
+>
+	<div class="relative rounded-xl">
+		<div class=" w-100 blur-xs rounded-4xl absolute inset-0 h-60 bg-blue-700"></div>
 		<img
 			src="/pic1.jpg"
 			alt="collab"
-			class="h-54 aspect-video w-96 rounded-xl shadow-blue-500/50 drop-shadow-xl"
+			class=" h-54 rounded-4xl relative z-10 mr-2 w-96 border-8 border-blue-800 object-cover"
 		/>
 	</div>
 	<div class="w-2xl flex-col">
-		<h1 class="text-4xl font-bold">Where Teams Connect, Collaborate, and Conquer.</h1>
+		<h1 class="py-6 text-5xl">Where Teams Connect, Collaborate, and Conquer.</h1>
 		<p class="text-justify text-xl">
 			SERAPHIC is a next-generation enterprise communication platform designed to streamline
 			collaboration, enhance productivity, and keep teams connected—no matter where they are. With
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Hero from '../components/Landing/Hero.svelte';
 </script>
 
-<div class="h-screen w-screen bg-[url('/Background.jpg')] pt-16">
+<div class="h-screen w-screen bg-[url('/Background.jpg')] pt-4">
 	<Hero />
 </div>
 
